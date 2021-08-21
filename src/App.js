ac6966608166bba2340200d0ddd420f5b02e2648
Bradly Lewis <bradlylewisdev@gmail.com>
2021-08-20 23:21:19 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,8 +9,3 @@ function App() {
 }
 
 export default App;
-
-// Unit 2 (and 3) will be in charge of creating the components
-// Unit 3 will provide authentication functionality for clients and instructors
-// Unit 4 builds the API (back-end)
-
