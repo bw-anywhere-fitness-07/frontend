2c132ac63d35d1f172a670c2bed0e5beb912bb61
Michael Steiner <michaelvsteiner@gmail.com>
2021-08-23 19:40:33 -0600

    Import Signup and update link in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
 
 // Components
-import NavBar from './Components/NavBar'
-import HomePage from './Components/HomePage'
+import NavBar from "./Components/NavBar";
+import HomePage from "./Components/HomePage";
+import Signup from "./Components/Signup";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Switch>
-        <Route exact path={'/'} component={HomePage}/>
-        <Route exact path={'/login'} />
-        <Route exact path={'/signup'} />
-
+        <Route exact path={"/"} component={HomePage} />
+        <Route exact path={"/login"} />
+        <Route exact path={"/signup"} component={Signup} />
       </Switch>
     </div>
   );
