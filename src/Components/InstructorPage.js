2452e8ff4eef29826767c10f602037a72b3cc773
Michael Steiner <michaelvsteiner@gmail.com>
2021-08-24 17:07:25 -0600

    Create InstructorPage component and InstuctorClasses component

--- src/Components/InstructorPage.js
+++ src/Components/InstructorPage.js
@@ -1,13 +1,27 @@
 // The Private instructor page
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import dummyData from "../dummy-data/classes";
+import InstructorClasses from "./InstructorClasses";
 
-const InstructorPage = () => {
+export default function InstructorPage() {
+  const [classes, setClasses] = useState([]);
+  const [myClasses, setMyClasses] = useState([]);
+
+  const myInstructorName = "Bob"; //not sure how this gets passed from instructor login?
+  useEffect(() => {
+    setClasses(dummyData);
+  }, []);
+  useEffect(() => {
+    setMyClasses(classes.filter((item) => item.instructor_name === myInstructorName));
+  }, [classes]);
+
   return (
     <div className="instructor-page-container">
+      <h2>My Classes</h2>
+      <button>Add a Class</button>
 
+      <InstructorClasses classes={myClasses} />
     </div>
   );
-};
-
-export default InstructorPage;
+}
